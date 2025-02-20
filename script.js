27b54328d7e9bee14f1de8cbc118c2d81209f720
zoyirr <zoyir6171@gmail.com>
2025-02-20 14:51:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// var a=20;
+// let ism="zoyirjon";
+// const yil=2025;
+// a=26
+// yil="2026"
+// console.log(yil);
+
+// confirm("davom etamz")
+
+let ism2=prompt("ismingizni kiriting")
+console.log("sizning ismingiz: "+ ism2);
+
+
+let ism3=prompt("familiyangizni kiriting")
+console.log("sizning familiyangiz: "+ ism3);
+
+
+let ism4=prompt("yoshizni kiriting")
+console.log("sizning yoshizni: "+ ism4);
+
+let ism5=prompt("qaysi maktabda o'qiysiz")
+console.log("sizning maktabiz: "+ ism5);
+
+
+let ism6=prompt("yoqtirgan faniz")
+console.log("yoqtirgan faniz: "+ ism6);
+
+let ism7=prompt("yashaydigan tumanizni kiriting")
+console.log("yashaydiga tumaniz: "+ ism7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+alert("malumotlar uchun rahmat!!!")
